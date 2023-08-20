29dd44e9c582b63746859fec4c1aff436b617d81
MaMbO1001 <loool123.rf@mail.ru>
2023-08-20 19:06:57 +0300

    Дз по срезам

--- main.go
+++ main.go
@@ -5,13 +5,38 @@ import (
 )
 
 func main() {
-	var test [5]int
-	test[0] = 2
-	test[1] = 4
-	test[2] = 6
-	test[3] = 8
-	test[4] = 10
-	for i := 0; i < len(test); i++ {
-		fmt.Println(test[i])
-	}
+	// Задание 1.
+	var zero []int = make([]int, 3)
+	zero[0] = 1
+	zero[1] = 2
+	zero[2] = 3
+	fmt.Println(zero)
+	// Задание 2.
+	var qwer []int = make([]int, 5)
+	qwer[0] = 1
+	qwer[1] = 2
+	qwer[2] = 3
+	qwer[3] = 4
+	qwer[4] = 5
+	qwer[4] = 8
+	fmt.Println(qwer)
+	// Задание 3.
+	var nnn []int
+	nnn = append(nnn, 1, 2, 3, 4, 6)
+	fmt.Println(nnn)
+	// Задание 4.
+	var x []int
+	x = append(x, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+	var c = x[1:3]
+	fmt.Println(c)
+	// Задание 5.
+	var b []int = make([]int, 5)
+	b[0] = 1
+	b[1] = 2
+	b[2] = 3
+	b[3] = 4
+	b[4] = 5
+	var v []int = make([]int, len(b))
+	copy(v, b)
+	fmt.Println(v)
 }
