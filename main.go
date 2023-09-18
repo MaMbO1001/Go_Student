eae55d160bff5464e91a9f37750063b6032309c3
MaMbO1001 <loool123.rf@mail.ru>
2023-09-19 01:04:04 +0300

    Раздел 2

--- main.go
+++ main.go
@@ -2,32 +2,76 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 func main() {
-	// Задание 1.
-	var test map[string]int
-	test = map[string]int{
-		"Viktor": 22,
-		"Ed": 28,
-		"Anna": 18,
-	}
-	fmt.Println(test)
-	// Задание 2
-	test["Alice"] = 23
-	test["Margo"] = 32
-	fmt.Println(test)
-	// Задание 3
-	delete(test, "Ed")
-	fmt.Println(test)
-	// Задание 4
-	a, b := test["Viktor"]
-	if b == true{
-		fmt.Println(a)
-	}
-	// Задание 5
-	for name, age := range test{
-		fmt.Printf("%s is %d years old//n", name, age)
-	}
-}
 
+	//Раздел 2. Задание 1.
+	var a int = 10
+	var b float64 = 77.1
+	var c string = "Name"
+	var isA bool = 4 == 0
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(isA)
+
+	// Задание 2.
+	var d int = 35
+	fmt.Println(a + d)
+	fmt.Println(a - d)
+	fmt.Println(a * d)
+	fmt.Println(d / a)
+
+	// Задание 3.
+	x := true
+	y := false
+	fmt.Println(x && y)
+	fmt.Println(x || y)
+
+	// Задание 4.
+	var g string = "10"
+	var h, err = strconv.Atoi(g)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(h)
+	}
+
+	// Задание 5 понятно.
+	// задание 6 понятно.
+
+	// Задание 7.
+	fmt.Println(!y)
+	fmt.Println(!x)
+
+	// Задание 8.
+	var o float64 = 17.9
+	var p int = int(o)
+	fmt.Println(p)
+	var l float64 = float64(p)
+	fmt.Println(l)
+
+	// Задание 9.
+	e := 10
+	fmt.Println(e)
+	// Мы не можем присвоить 2 значения одной переменной.
+
+	// Задание 10.
+	var j int
+	var u string
+	var t int
+	t, err = strconv.Atoi(u)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(t)
+	}
+
+	fmt.Println("Калькулятор сложения")
+	fmt.Scan(&t)
+	fmt.Scan(&j)
+	k := t + j
+	fmt.Println(k)
+}
