73e54f9c3ed894ef2cfa35830fe6bfa4f811257f
MaMbO1001 <loool123.rf@mail.ru>
2023-09-19 02:11:41 +0300

    Раздел 3

--- main.go
+++ main.go
@@ -2,76 +2,39 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
+	const Pi = 3.14159
+	const q = 14
+	const e = 45.99
+	const r = "Book"
+	const w = true
 
-func main() {
 
-	//Раздел 2. Задание 1.
-	var a int = 10
-	var b float64 = 77.1
-	var c string = "Name"
-	var isA bool = 4 == 0
-	fmt.Println(a)
-	fmt.Println(b)
-	fmt.Println(c)
-	fmt.Println(isA)
+func main () {
 
-	// Задание 2.
-	var d int = 35
-	fmt.Println(a + d)
-	fmt.Println(a - d)
-	fmt.Println(a * d)
-	fmt.Println(d / a)
+	// Раздел 3. Константы. Задание 1.
+    fmt.Println(Pi)
+
+    // Задание 2.
+	const Beta = 100
+	fmt.Println(Beta)
 
 	// Задание 3.
-	x := true
-	y := false
-	fmt.Println(x && y)
-	fmt.Println(x || y)
+    const Pi = 3.11111
+	fmt.Println(Pi, Pi)
 
 	// Задание 4.
-	var g string = "10"
-	var h, err = strconv.Atoi(g)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println(h)
-	}
+	fmt.Println(Pi + e)
+	fmt.Println(e + Pi)
+	fmt.Println(e * Pi)
+	fmt.Println(e / Pi)
 
-	// Задание 5 понятно.
-	// задание 6 понятно.
+	// Задание 5.
+	// Константы не могут быть изменены.
 
-	// Задание 7.
-	fmt.Println(!y)
-	fmt.Println(!x)
-
-	// Задание 8.
-	var o float64 = 17.9
-	var p int = int(o)
-	fmt.Println(p)
-	var l float64 = float64(p)
-	fmt.Println(l)
-
-	// Задание 9.
-	e := 10
-	fmt.Println(e)
-	// Мы не можем присвоить 2 значения одной переменной.
-
-	// Задание 10.
-	var j int
-	var u string
-	var t int
-	t, err = strconv.Atoi(u)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println(t)
-	}
-
-	fmt.Println("Калькулятор сложения")
-	fmt.Scan(&t)
-	fmt.Scan(&j)
-	k := t + j
-	fmt.Println(k)
+	// Задание 6.
+	fmt.Println("Мне", q, "лет")
+	fmt.Println("Размер ноги:", e)
+	fmt.Println("Беру на учёбу:", r)
+	fmt.Println(w)
 }
